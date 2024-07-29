b1d10d33da37e403f2dedbbb112e82df0c916cc3
Lingzhe Zhao <zhaolingzhe@westlake.edu.cn>
2024-07-29 20:23:20 +0800

    create_user: minor

--- scripts/create_user.py
+++ scripts/create_user.py
@@ -2,16 +2,23 @@
 
 import requests
 from io import StringIO
+from os import getenv
+
 from fabric import Connection, Config
 from my_secrets import TRUENAS_USERNAME, TRUENAS_PASSWORD, SUDO_PASSWORD, DET_PASSWORD, HARBOR_PASSWORD
+
 
 TRUENAS_API_URL = "http://10.0.1.70/api/v2.0"
 HARBOR_API_URL = "http://10.0.1.68:50000/api/v2.0"
 
+connect_kwargs = {
+    'passphrase': getenv('SSH_PASSPHRASE')  # SSH private key passphrase
+}
+
 def create_user_login_node(username, password):
     # Connect to the server
     config = Config(overrides={'sudo': {'password': SUDO_PASSWORD}})
-    conn = Connection("login", config=config)
+    conn = Connection("cvgladmin@login", config=config, connect_kwargs=connect_kwargs)
 
     # Create the user
     conn.sudo(f"useradd -m -s /bin/bash {username}")
@@ -137,19 +144,26 @@ def create_home_nfs_share_truenas(username):
 def mount_home_all(username):
     # Connect to the server
     config = Config(overrides={'sudo': {'password': SUDO_PASSWORD}})
-    for server in ["login", "S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8"]:
-        conn = Connection(server, config=config)
+    for server in ["cvgladmin@login","S1", "S2", "S3", "S4", "S5", "S6","S7", "S8"]:
+        conn = Connection(server, config=config, connect_kwargs=connect_kwargs)
 
         # Mount the NFS share
         mount_sources = [f"nas.cvgl.lab:/mnt/Peter/Workspace/{username}"]
-        mount_targets = ["/workspace/{username}"]
+        mount_targets = [f"/workspace/{username}"]
 
-        if server == "login":
+        if "login" in server:
             mount_sources.append(mount_sources[0])
             mount_targets.append(f"/home/{username}")
 
         for mount_source, mount_target in zip(mount_sources, mount_targets):
             conn.sudo(f"mkdir -p {mount_target}")
+            # # remove the following lines from /etc/fstab
+            # to_rm = [
+            #     # ...
+            # ]
+            # for line in to_rm:
+            #     conn.sudo(f"bash -c \"sed -i '#{line}#d' /etc/fstab\"")
+
             # append the following line to /etc/fstab
             mount_string = f"{mount_source} {mount_target} nfs defaults,vers=3,async,noatime,soft,rsize=32769,wsize=32768,_netdev 0 2"
             conn.sudo(f"bash -c 'echo {mount_string} >> /etc/fstab'")
@@ -159,7 +173,7 @@ def mount_home_all(username):
 def generate_home_contents_login_node(username, password):
     # Connect to the server
     config = Config(overrides={'sudo': {'password': SUDO_PASSWORD}})
-    conn = Connection("login", config=config)
+    conn = Connection("cvgladmin@login", config=config, connect_kwargs=connect_kwargs)
 
     # Update the user's home directory
     conn.sudo("xdg-user-dirs-update --force", user=username)
@@ -170,14 +184,16 @@ def generate_home_contents_login_node(username, password):
         pass
 
 
-def create_user_det(username, password, uid, gid):
+def create_user_det(username, password, uid, gid, fullname=None):
     # Connect to the server
-    conn = Connection("login")
+    conn = Connection("cvgladmin@login")
 
     # Login to the Determined CLI
     conn.run("det user login admin", in_stream=StringIO(f"{DET_PASSWORD}\n"))
     conn.run(f"det user create {username} --password {password}")
     conn.run(f"det user link-with-agent-user {username} --agent-uid {uid} --agent-user {username} --agent-gid {gid} --agent-group {username}")
+    if fullname:
+        conn.run(f"det user edit {username} --display-name {fullname}")
     conn.run("det user list")
 
 
@@ -213,7 +229,7 @@ def add_user_to_harbor_project_member(username):
     response.raise_for_status()
 
 
-def create_user(username, password):
+def create_user(username, password, fullname=None):
     uid, gid = create_user_login_node(username, password)
     create_group_truenas(username, gid)
     create_user_truenas(username, uid)
@@ -222,18 +238,22 @@ def create_user(username, password):
     create_home_nfs_share_truenas(username)
     mount_home_all(username)
     generate_home_contents_login_node(username, password)
-    create_user_det(username, password, uid, gid)
+    create_user_det(username, password, uid, gid, fullname)
     create_user_harbor(username, password)
     add_user_to_harbor_project_member(username)
 
 usernames = [
-    # fill in the usernames here
+    # fill in the usernames
+]
+
+full_names = [
+    # fill in the full names
 ]
 
 passwords = [
-    # fill in the passwords here
+    # fill in the passwords
 ]
 
-for username, password in zip(usernames, passwords):
-    create_user(username, password)
+for username, password, fullname in zip(usernames, passwords, full_names):
+    create_user(username, password, fullname)
     print(f"User {username} created successfully.")
